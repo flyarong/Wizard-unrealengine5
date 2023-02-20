1caf50ac19308e73876284447ce57423c62c1b73
Amrein Milán <amrein7@gmail.com>
2023-02-20 09:29:36 +0100

    File rearrangement for Cursor, SpellMap, MiniMap etc.

--- Source/Wizard/HUD/WizardWidgetClasses/Combat/SpellMapWidget.cpp
+++ Source/Wizard/HUD/WizardWidgetClasses/Combat/SpellMapWidget.cpp
@@ -7,13 +7,13 @@
 void USpellMapWidget::ConstructSpellMap(TMap<FKey, int32>& SpellMap)
 {
 	for (auto& Spell : SpellMap) {
-		if (Spell.Key == EKeys::Up) {
+		if (Spell.Key == EKeys::W) {
 			TopSymbol->SetBrushFromTexture(Symbols[Spell.Value]);
-		} else if (Spell.Key == EKeys::Left) {
+		} else if (Spell.Key == EKeys::A) {
 			LeftSymbol->SetBrushFromTexture(Symbols[Spell.Value]);
-		} else if (Spell.Key == EKeys::Down) {
+		} else if (Spell.Key == EKeys::S) {
 			BottomSymbol->SetBrushFromTexture(Symbols[Spell.Value]);
-		} else if (Spell.Key == EKeys::Right) {
+		} else if (Spell.Key == EKeys::D) {
 			RightSymbol->SetBrushFromTexture(Symbols[Spell.Value]);
 		}
 	}
