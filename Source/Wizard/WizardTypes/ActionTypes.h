7c63da160175b99e5f3d8e5cdab4223f3b776732
Amrein Milán <amrein7@gmail.com>
2023-02-24 12:12:14 +0100

    Added Combat to ActionTypes

--- Source/Wizard/WizardTypes/ActionTypes.h
+++ Source/Wizard/WizardTypes/ActionTypes.h
@@ -4,6 +4,7 @@ UENUM(BlueprintType)
 enum class EAction : uint8
 {
 	EA_Movement UMETA(DisplayName = "Movement"),
+	EA_Combat UMETA(DisplayName = "Combat"),
 
 	EA_MAX UMETA(DisplayName = "DefaultMAX")
 };
