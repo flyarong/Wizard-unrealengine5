ce4237e3ee9d3ad1803019be16c67b0d52255063
Amrein Milán <amrein7@gmail.com>
2023-03-02 10:15:16 +0100

    Added XP to Boost Types

--- Source/Wizard/WizardTypes/BoostTypes.h
+++ Source/Wizard/WizardTypes/BoostTypes.h
@@ -6,6 +6,7 @@ enum class EBoost : uint8
 	EB_Health UMETA(DisplayName = "Health"),
 	EB_Defense UMETA(DisplayName = "Defense"),
 	EB_Power UMETA(DisplayName = "Power"),
+	EB_XP UMETA(DisplayName = "XP"),
 	EB_Wisdom UMETA(DisplayName = "Wisdom"),
 	EB_Intelligence UMETA(DisplayName = "Intelligence"),
 	EB_Offense UMETA(DisplayName = "Offense"),
